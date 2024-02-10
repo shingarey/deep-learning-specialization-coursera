90cfab097eb81964daf0b6579617878ec6dfc443
Dmitriy Shingarey <shingarey@gmail.com>
2024-02-10 20:36:51 +0100

    add week 3 solutions

---
+++ C1_Neural_networks_and_deep_learning/W3_shallow_neural_network/programming_assignment/planar_data_classification_with_one_hidden_layer.py
@@ -0,0 +1,107 @@
+# Package imports
+import numpy as np
+import copy
+import matplotlib.pyplot as plt
+from testCases_v2 import *
+from public_tests import *
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
+
+%matplotlib inline
+
+%load_ext autoreload
+%autoreload 2
+
+
+X, Y = load_planar_dataset()
+
+# Visualize the data:
+plt.scatter(X[0, :], X[1, :], c=Y, s=40, cmap=plt.cm.Spectral);
+
+
+# GRADED FUNCTION: layer_sizes
+
+def layer_sizes(X, Y):
+    """
+    Arguments:
+    X -- input dataset of shape (input size, number of examples)
+    Y -- labels of shape (output size, number of examples)
+    
+    Returns:
+    n_x -- the size of the input layer
+    n_h -- the size of the hidden layer
+    n_y -- the size of the output layer
+    """
+    #(≈ 3 lines of code)
+    # n_x = ... 
+    # n_h = ...
+    # n_y = ... 
+    # YOUR CODE STARTS HERE
+    n_x = X.shape[0]
+    n_h = 4
+    n_y = Y.shape[0]
+    
+    # YOUR CODE ENDS HERE
+    return (n_x, n_h, n_y)
+
+
+t_X, t_Y = layer_sizes_test_case()
+(n_x, n_h, n_y) = layer_sizes(t_X, t_Y)
+print("The size of the input layer is: n_x = " + str(n_x))
+print("The size of the hidden layer is: n_h = " + str(n_h))
+print("The size of the output layer is: n_y = " + str(n_y))
+
+layer_sizes_test(layer_sizes)
+
+
+# GRADED FUNCTION: initialize_parameters
+
+def initialize_parameters(n_x, n_h, n_y):
+    """
+    Argument:
+    n_x -- size of the input layer
+    n_h -- size of the hidden layer
+    n_y -- size of the output layer
+    
+    Returns:
+    params -- python dictionary containing your parameters:
+                    W1 -- weight matrix of shape (n_h, n_x)
+                    b1 -- bias vector of shape (n_h, 1)
+                    W2 -- weight matrix of shape (n_y, n_h)
+                    b2 -- bias vector of shape (n_y, 1)
+    """    
+    #(≈ 4 lines of code)
+    # W1 = ...
+    # b1 = ...
+    # W2 = ...
+    # b2 = ...
+    # YOUR CODE STARTS HERE
+    
+    W1 = np.random.randn(n_h,n_x) * 0.01
+    b1 = np.zeros((n_h,1))
+    W2 = np.random.randn(n_y,n_h) * 0.01
+    b2 = np.zeros((n_y,1))
+    
+    # YOUR CODE ENDS HERE
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
+
+
+np.random.seed(2)
+n_x, n_h, n_y = initialize_parameters_test_case()
+parameters = initialize_parameters(n_x, n_h, n_y)
+
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+
+initialize_parameters_test(initialize_parameters)
+
