2896949713da68929565f0b2538947e3c22b9c68
Dmitriy Shingarey <shingarey@gmail.com>
2024-02-12 00:02:28 +0100

    update week 4 assignments

---
+++ C1_Neural_networks_and_deep_learning/W4_deep_neural_network/programming_assignments/W4A1/building_your_neep_neural_network_step_by_step.py
@@ -0,0 +1,17 @@
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from testCases import *
+from dnn_utils import sigmoid, sigmoid_backward, relu, relu_backward
+from public_tests import *
+
+import copy
+%matplotlib inline
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+%load_ext autoreload
+%autoreload 2
+
+np.random.seed(1)
